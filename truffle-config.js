27953c3ba895190f2c29a292db7d42dd178e7244
Ryo Sato <ryo.sato@datachain.jp>
2021-12-03 20:03:23 +0900

    change truffle-config for harmony
    Signed-off-by: Ryo Sato <ryo.sato@datachain.jp>

--- truffle-config.js
+++ truffle-config.js
@@ -1,72 +1,69 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * trufflesuite.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
+//require('dotenv').config()
+const { TruffleProvider } = require('@harmony-js/core')
+//Local
+const local_mnemonic = process.env.LOCAL_MNEMONIC
+const local_private_key = process.env.LOCAL_PRIVATE_KEY
+const local_url = process.env.LOCAL_0_URL;
+//Testnet
+const testnet_mnemonic = process.env.TESTNET_MNEMONIC
+const testnet_private_key = process.env.TESTNET_PRIVATE_KEY
+const testnet_url = process.env.TESTNET_0_URL
+//const testnet_0_url = process.env.TESTNET_0_URL
+//const testnet_1_url = process.env.TESTNET_1_URL
+//Mainnet
+const mainnet_mnemonic = process.env.MAINNET_MNEMONIC
+const mainnet_private_key = process.env.MAINNET_PRIVATE_KEY
+const mainnet_url = process.env.MAINNET_0_URL;
 
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-var mnemonic = "math razor capable expose worth grape metal sunset metal sudden usage scheme";
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+//GAS - Currently using same GAS accross all environments
+gasLimit = process.env.GAS_LIMIT
+gasPrice = process.env.GAS_PRICE
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
 
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
-    //  gas: 100000000,
-     provider: () =>
-      new HDWalletProvider(mnemonic, "http://localhost:8545", 0, 10)
+    local: {
+      network_id: '2',
+      provider: () => {
+        const truffleProvider = new TruffleProvider(
+          local_url,
+          { memonic: local_mnemonic },
+          { shardID: 0, chainId: 2 },
+          { gasLimit: gasLimit, gasPrice: gasPrice},
+        );
+        const newAcc = truffleProvider.addByPrivateKey(local_private_key);
+        truffleProvider.setSigner(newAcc);
+        return truffleProvider;
+      },
     },
-    testchain0: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8645,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-      // gas: 100000000,
-      provider: () =>
-       new HDWalletProvider(mnemonic, "http://localhost:8645", 0, 10)
-     },
-     testchain1: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8745,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-      // gas: 100000000,
-      provider: () =>
-       new HDWalletProvider(mnemonic, "http://localhost:8745", 0, 10)
-     }
+    testnet: {
+      network_id: '2',
+      provider: () => {
+        const truffleProvider = new TruffleProvider(
+          testnet_url,
+          { memonic: testnet_mnemonic },
+          { shardID: 0, chainId: 2 },
+          { gasLimit: gasLimit, gasPrice: gasPrice},
+        );
+        const newAcc = truffleProvider.addByPrivateKey(testnet_private_key);
+        truffleProvider.setSigner(newAcc);
+        return truffleProvider;
+      },
+    },
+    mainnet0: {
+      network_id: '1',
+      provider: () => {
+        const truffleProvider = new TruffleProvider(
+          mainnet_url,
+          { memonic: mainnet_mnemonic },
+          { shardID: 0, chainId: 1 },
+          { gasLimit: gasLimit, gasPrice: gasPrice },
+        );
+        const newAcc = truffleProvider.addByPrivateKey(mainnet_private_key);
+        truffleProvider.setSigner(newAcc);
+        return truffleProvider;
+      },
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -80,16 +77,12 @@ module.exports = {
       version: "0.8.9",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
-       optimizer: {
-         enabled: true,
-         runs: 1000
-       },
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      },
       //  evmVersion: "byzantium"
       }
     }
-  },
-
-  plugins: [
-    'truffle-contract-size'
-  ]
-};
+  }
+}
